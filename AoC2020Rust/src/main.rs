89c5b98ec384dc1975deae10f66880802ecc5814
JacobPuff <jacob@todandlorna.com>
2020-12-01 09:43:53 -0700

    Get setup for day 2.

--- AoC2020Rust/src/main.rs
+++ AoC2020Rust/src/main.rs
@@ -1,10 +1,11 @@
 mod days;
 
 fn main() {
-    let day: i32 = 1;
+    let day: i32 = 2;
     println!("Running day {}", day);
     match day {
         1 => days::day1::day1(),
+        2 => days::day1::day1(),
         _ => println!("We dont have that day.")
     }
 }
