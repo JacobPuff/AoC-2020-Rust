00af731e3fa1a12edb79b7dfbd1db4e9f536060e
JacobPuff <jacob@todandlorna.com>
2020-12-01 23:12:21 -0700

    remove target folder

--- AoC2020Rust/target/debug/deps/AoC2020Rust-b2dbc4986fd6a526.d
+++
@@ -1,8 +0,0 @@
-/mnt/c/Users/Jacob/Documents/repos/Advent of code 2020 Rust/AoC2020Rust/target/debug/deps/AoC2020Rust-b2dbc4986fd6a526: src/main.rs src/days/mod.rs src/days/day1.rs src/days/day2.rs
-
-/mnt/c/Users/Jacob/Documents/repos/Advent of code 2020 Rust/AoC2020Rust/target/debug/deps/AoC2020Rust-b2dbc4986fd6a526.d: src/main.rs src/days/mod.rs src/days/day1.rs src/days/day2.rs
-
-src/main.rs:
-src/days/mod.rs:
-src/days/day1.rs:
-src/days/day2.rs:
