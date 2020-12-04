5658d1f521407199fd4adb705ebe501053b207d7
JacobPuff <jacob@todandlorna.com>
2020-12-04 10:55:33 -0700

    Complete day 4

--- AoC2020Rust/src/main.rs
+++ AoC2020Rust/src/main.rs
@@ -1,12 +1,13 @@
 mod days;
 
 fn main() {
-    let day: i32 = 3;
+    let day: i32 = 4;
     println!("Running day {}", day);
     match day {
         1 => days::day1::day1(),
         2 => days::day2::day2(),
         3 => days::day3::day3(),
+        4 => days::day4::day4(),
         _ => println!("We dont have that day.")
     }
 }
