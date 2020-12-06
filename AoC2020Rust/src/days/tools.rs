0de27273d759450d9efac9736f1d20e0e425f8cc
JacobPuff <jacob@todandlorna.com>
2020-12-06 14:02:32 -0700

    Get input, but in a more differenter way.

--- AoC2020Rust/src/days/tools.rs
+++ AoC2020Rust/src/days/tools.rs
@@ -3,6 +3,6 @@ use std::io::{self, BufRead};
 pub fn read_from_stdin(prompt: &str) -> Option<String> {
     println!("{}", prompt);
     let stdin = io::stdin();
-    let line_one = &stdin.lock().lines().next().unwrap().unwrap();
-    Some(line_one.to_string())
+    let input = stdin.lock().lines().next()?.expect("Found no input");
+    Some(input.to_string())
 }
