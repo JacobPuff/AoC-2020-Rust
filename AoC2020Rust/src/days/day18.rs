a2beeef25a565850c5f372effa03037d70a87b2a
JacobPuff <jacob@todandlorna.com>
2021-01-05 17:31:07 -0700

    Clean up day 18

--- AoC2020Rust/src/days/day18.rs
+++ AoC2020Rust/src/days/day18.rs
@@ -10,19 +10,6 @@ struct Node {
     operation: &'static str
 }
 
-// impl Copy for Node {
-//     fn Copy(&self, other: &Self) -> Node {
-//         let copied_node: Node = Node {
-//             id: self.id,
-//             parent: self.parent,
-//             children: self.children.to_vec(),
-//             value: self.value,
-//             operation: self.operation
-//         };
-//         return copied_node
-//     }
-// }
-
 pub fn day18() {
     part_one();
     part_two();
@@ -102,13 +89,8 @@ fn part_two() {
         let mut current_priority = 1;
         let mut max_priority = 0;
         for c in line.chars() {
-            // if c != ' ' {
-            //     println!("Current Parent is {} c is {}", current_parent, c);
-            // }
             match c {
                 '(' => {
-                    // 2+(3+4)*(5+6)
-                    // Always add 2, because the first num in the first paren has higher priority.
                     current_priority += 2;
                     layer_starts.push(current_id);
                     layer_starts.push(current_id);
@@ -120,32 +102,11 @@ fn part_two() {
                 },
                 ' ' => (),
                 '+' => {
-                    // Try to keep these at the same priority.
+                    // Keep each layer at the same priority
                     if current_priority as usize <= layer_starts.len()-1 {
                         current_priority += 1;
                     }
                     current_operation = "+";
-
-                    // These stopped being about the + operation, and became a general list.
-
-                    // Priority is only set back by one, and not set back to the start. CHECKED
-                    // How can we check that this has only be raised once? CHECKED
-                    // Can we guarantee that a node exists, or should we just push priority? CHECKED
-                    // In input 1 + (2 * 3) + (4 * (5 + 6)):
-                    //   Priority matches up between 3 and 4. CHECKED this is actually ok.
-                    //   Parent of 4 is 3, but should be 2. When the parent is set to 2,
-                    //       once 4 is reached the parent is set to current id, which is 3. CHECKED
-
-
-                    // TODO
-                    // Walk through the program with 1 + (2 * 3) + (4 * (5 + 6)) as input to figure
-                    //     out why double parens break things. CHECKED
-                    // Fix double parens. CHECKED not actually the problem.
-                    // Fix parens next to eachother. CHECKED they now have correct parents.
-                    // Fix order of operations. 4 should not add to 2 before 3 is multiplied. CHECKED
-                    // In input ((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2:
-                    //   The last 3 nodes are not processed. CHECKED
-                    // Walk through program to solve above.
                 },
                 '*' => {
                     current_operation = "*";
@@ -175,16 +136,12 @@ fn part_two() {
                                 let potential_parent = layer_starts.pop().unwrap();
                                 if potential_parent != current_id {
                                     current_parent = potential_parent;
-                                    // println!("{:?}", node_map[&current_parent]);
-                                    // current_priority = node_map[&current_parent].priority;
-                                    // println!("{}", current_parent);
                                     layer_starts.push(current_id);
                                 } else {
                                     layer_starts.push(potential_parent);
                                 }
                             }
                         }
-                        // println!("{} CPy", current_priority);
                         if current_priority > max_priority {
                             max_priority = current_priority;
                         }
@@ -204,7 +161,6 @@ fn part_two() {
                             current_priority += beginning_of_paren_offset;
                         }
 
-                        // println!("{} CPy", current_priority);
                         current_parent = current_id;
                         current_id += 1;
                     } else {
@@ -213,41 +169,21 @@ fn part_two() {
                 }
             }
         }
-        // println!("Yeet");
-        // println!("{:?}", node_map);
+
         for p_offset in 0..=max_priority {
-            // let mut new_nodes: Vec<Node> = Vec::new();
-            // reversed_node_map.reverse();
-            // let mut node_loop_vec = &mut node_map;
-            // node_loop_vec.push(node_map[node_map.len()-1]);
             for i in 0..node_map.len() {
                 let node = node_map[&((node_map.len()-i-1) as i64)];
                 if node.priority == max_priority - p_offset && node.id != trunk_id {
-                    // println!("current_node is {:?}", node);
-
-                    // If 2 add into 1, and 1 is the same priority, than 1 wont be changed
-                    // for the next operation CHECKED
                     let mut parent_node = node_map[&node.parent];
-                    // println!("{:?}", node);
-                    // println!("op: {}", node.operation);
-                    // println!("pr: {}", parent_node.value);
                     match node.operation {
                         "+" => parent_node.value += node.value,
                         "*" => parent_node.value *= node.value,
                         _=>()
                     }
-                    // println!("pr: {}\n", parent_node.value);
-
-                    // node_map.remove(&parent_node.id);
                     node_map.insert(parent_node.id, parent_node);
-                    // println!("{:?}", node_map);
                 }
             }
-            // new_nodes.reverse();
-            // node_map = new_nodes;
         }
-        // println!("{:?}", node_map);
-
         let result_of_line = node_map[&trunk_id].value;
         part_two_sum_of_line_results += result_of_line;
     }
